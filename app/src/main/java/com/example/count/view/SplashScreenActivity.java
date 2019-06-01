34bfc45116da8e8bc2f21869dc539bae389d35dc
Reeshabh Ranjan <reeshabhkumarranjan@gmail.com>
2019-06-01 12:48:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/count/view/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.count.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.count.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int delayDuration=500;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Class nextActivityClass = getNextClass();
+                Intent intent = new Intent(SplashScreenActivity.this, nextActivityClass);
+                finish(); // so that the back button doesn't bring back to the SplashScreen
+            }
+
+            private Class getNextClass() {
+                // TODO complete this method based on logged in status
+                return null;
+            }
+        },delayDuration);
+    }
+}
