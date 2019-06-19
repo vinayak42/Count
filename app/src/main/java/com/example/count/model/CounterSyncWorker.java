450b1cd686b2286a25fc9b95967bf14f788cc181
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-06-20 00:25:45 +0530

    Added code to show notification

--- app/src/main/java/com/example/count/model/CounterSyncWorker.java
+++ app/src/main/java/com/example/count/model/CounterSyncWorker.java
@@ -1,13 +1,20 @@
 package com.example.count.model;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.os.Build;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationBuilderWithBuilderAccessor;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import com.example.count.R;
 import com.example.count.view.Counter;
 import com.example.count.view.CounterRepository;
 import com.example.count.view.SettingsActivity;
@@ -22,6 +29,40 @@ import java.util.List;
 import java.util.Map;
 
 public final class CounterSyncWorker extends Worker {
+
+    private final String NOTIFICATION_CHANNEL_ID = "counterNotification";
+    private final int NOTIFICATION_ID = 100;
+
+    private void showNotification(boolean success) {
+
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_plus_one_black_24dp)
+                .setContentTitle("Sync successful")
+                .setContentText("Your data was successfully uploaded to server")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = NOTIFICATION_CHANNEL_ID;
+            String description = "";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     public CounterSyncWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
