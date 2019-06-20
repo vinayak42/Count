1ecec651fc49bc8f4cb08f5bdab0e91a16116f0b
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-06-20 10:52:43 +0530

    Added code for creating notification channel in splash screen itself.

--- app/src/main/java/com/example/count/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/count/view/SplashScreenActivity.java
@@ -2,7 +2,11 @@ package com.example.count.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -10,10 +14,29 @@ import com.example.count.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private final String NOTIFICATION_CHANNEL_ID = "counterNotification";
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = NOTIFICATION_CHANNEL_ID;
+            String description = "";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        createNotificationChannel();
 
         int delayDuration=500;
 
