e7eaadfe24730b121165afe7f1ee19dc14246f8e
Reeshabh Kumar Ranjan <reeshabhkumarranjan@gmail.com>
2019-06-21 13:23:01 +0530

    Added code to sync data only if it has been 24 hours since last sync (untested).

--- app/src/main/java/com/example/count/view/SettingsActivity.java
+++ app/src/main/java/com/example/count/view/SettingsActivity.java
@@ -20,6 +20,7 @@ import com.example.count.model.Utils;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -27,14 +28,18 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private static final long MILLISECONDS_IN_A_DAY = 86400000;
+    private static final long MILLISECONDS_IN_AN_HOUR = 3600000;
     private Button syncButton;
     private Button deleteAllCountersButton;
+    private boolean allowed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +95,15 @@ public class SettingsActivity extends AppCompatActivity {
                 boolean connected = checkInternetConnection();
                 boolean syncAllowed = checkLastSyncTimestamp();
 
+                if (!connected) {
+                    Toast.makeText(SettingsActivity.this, "Please connect to internet first.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!syncAllowed) {
+                    return;
+                }
+
                 FirebaseFirestore db = Utils.getInstance().getDb();
                 FirebaseUser user = Utils.getInstance().getUser();
                 CounterRepository counterRepository = new CounterRepository(getApplication());
@@ -118,12 +132,51 @@ public class SettingsActivity extends AppCompatActivity {
                         }
                     });
                 }
+
+                updateLastSyncTimeOnServer();
             }
         });
     }
 
+    private void updateLastSyncTimeOnServer() {
+        FirebaseFirestore db = Utils.getInstance().getDb();
+        FirebaseUser user = Utils.getInstance().getUser();
+        DocumentReference userDocumentReference = db.collection("users").document(user.getUid());
+        Map<String, Object> syncTimeUpdationObject = new HashMap<>();
+        syncTimeUpdationObject.put("last_sync_timestamp", new Timestamp(new Date()));
+        userDocumentReference.update(syncTimeUpdationObject);
+    }
+
     private boolean checkLastSyncTimestamp() {
-        return false;
+        FirebaseFirestore db = Utils.getInstance().getDb();
+        FirebaseUser user = Utils.getInstance().getUser();
+        DocumentReference syntTimestampReference = db.collection("users").document(user.getUid());
+        syntTimestampReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        HashMap<String, Object> retreivedData = (HashMap<String, Object>) documentSnapshot.getData();
+                        Timestamp lastSyncTimestamp = (Timestamp) retreivedData.get("last_sync_timestamp");
+                        Date lastSyncDate = lastSyncTimestamp.toDate();
+                        Date currentDate = new Date();
+                        long diff = currentDate.getTime() - lastSyncDate.getTime();
+
+                        if (diff < MILLISECONDS_IN_A_DAY) {
+                            allowed = false;
+                            long hoursToWait = (long)(Math.ceil(diff / (double) MILLISECONDS_IN_AN_HOUR));
+                            Toast.makeText(SettingsActivity.this, "Please wait " + hoursToWait + " hour(s) before syncing again.", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            allowed = true;
+                        }
+                    }
+                }
+            }
+        });
+
+        return allowed;
     }
 
     private boolean checkInternetConnection() {
